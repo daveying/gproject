0b92452af89274fa40e77deb1cd9971f0509b47e
daveying <daveying@163.com>
2017-03-24 22:03:46 +0800

    remove build/ and devel/

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aimm/gproject/src
-# Build directory: /home/aimm/gproject/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
